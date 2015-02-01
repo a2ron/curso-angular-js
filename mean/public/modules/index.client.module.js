323eba121d2aa843512b73aa9e3281cd3756388a
Aarón Rosas <aarr90@gmail.com>
2015-02-01 18:35:55 +0100

    #1. 01:00H. Block interface (index.js) in not User logged. NavBar (links and active).

--- mean/public/modules/index.client.module.js
+++ mean/public/modules/index.client.module.js
@@ -1,6 +1,21 @@
 angular.module('index', [])
-        .controller('IndexController', ['$scope',
-            function($scope)
+        .controller('IndexController', ['$scope', '$location',
+            function($scope, $location)
             {
+                $scope.menu = [
+                    {
+                        "title": "Apuntes",
+                        "path": "apuntes"
+                    }, {
+                        "title": "Categorias",
+                        "path": "categorias"
+                    }];
+
                 $scope.user = window.user;
+
+                $scope.menuItemActive = function(_path)
+                {
+                    var path = $location.$$path.split('/');
+                    return (path[1] && path[1] === _path) ? 'active' : '';
+                };
             }]);
