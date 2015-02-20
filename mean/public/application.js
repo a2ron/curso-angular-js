4a2880f74a962a3347d41bded91bfa8d9a0bc511
Aarón Rosas <aarr90@gmail.com>
2015-02-20 18:59:10 +0100

    /* For initialice format in datepickerPopup*/

--- mean/public/application.js
+++ mean/public/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'ngResource', 'index', 'apuntes', 'categoriasApuntes','ngLayout','ngGrid','ui.bootstrap']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'ngResource', 'index', 'apuntes', 'categoriasApuntes', 'ngLayout', 'ngGrid', 'ui.bootstrap']);
 
 mainApplicationModule.config(['$locationProvider',
     function($locationProvider)
@@ -12,3 +12,19 @@ angular.element(document).ready(function()
 {
     angular.bootstrap(document, [mainApplicationModuleName]);
 });
+
+/* For initialice format in datepickerPopup*/
+angular.module(mainApplicationModuleName).directive('datepickerPopup', function(dateFilter, datepickerPopupConfig) {
+    return {
+        restrict: 'A',
+        priority: 1,
+        require: 'ngModel',
+        link: function(scope, element, attr, ngModel) {
+            var dateFormat = attr.datepickerPopup || datepickerPopupConfig.datepickerPopup;
+            ngModel.$formatters.push(function(value) {
+                return dateFilter(value, dateFormat);
+            });
+        }
+    };
+});
+
